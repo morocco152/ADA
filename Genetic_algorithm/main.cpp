70a77f41399fb6b17046c763d24e13029e3203e7
morocco152 <58355129+morocco152@users.noreply.github.com>
2021-06-08 14:29:35 -0500

    Update main.cpp

--- Genetic_algorithm/main.cpp
+++ Genetic_algorithm/main.cpp
@@ -7,6 +7,8 @@ int main(int argc, char const *argv[]){
     std::vector<int> array = {6,5,1,2,4,3};
     Genetic<int> test = Genetic<int>(array,0.3,5,3);
     test.iniciar();
+
+    array.clear();
   
     return EXIT_SUCCESS;
 }
