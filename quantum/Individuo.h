24c682c8cf9960d829c650d671a925d9ecbe9d8f
morocco152 <58355129+morocco152@users.noreply.github.com>
2021-07-18 16:24:15 -0500

    Update Individuo.h

--- quantum/Individuo.h
+++ quantum/Individuo.h
@@ -6,19 +6,19 @@ class Individuo
     friend class Genetic;
     private:
         Qubit* cromosoma;
-        int* cromo_bit;
+        int* cromosome_colapsed;
         float fitness;
         size_t size;
         int profit;
         int weight;
 
     public:
+        Individuo(size_t size);
         bool operator < (const Individuo& o) const;
         void evaluate_fitness(const int);
-        Qubit& operator [] (int index);
-        Individuo(size_t size);
+        Qubit& operator[](int index); 
+        const int& operator[] (int index) const;
         void observate();
-        ~Individuo();
 };
 
 #include "Individuo.cpp"
