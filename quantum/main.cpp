baf071c87296fe4366fcf4d9c6af81f9fc3e8f4d
morocco152 <58355129+morocco152@users.noreply.github.com>
2021-07-18 16:22:24 -0500

    Update main.cpp

--- quantum/main.cpp
+++ quantum/main.cpp
@@ -5,20 +5,10 @@ int main(int argc, char **argv)
 {
     //Algorithm
     srand(static_cast<unsigned int>(time(nullptr)));
-    Genetic a(20,10,10,100);
+    Genetic a(5,10,1000,20);
     a.exe();
 
     //free memory
     a.~Genetic();
     return EXIT_SUCCESS;
-
-    /* INFORME 
-     * - Debemos mejorar la función fitness
-     * - Repair debe ser implementada
-     * - Debemos asegurarnos que los datos se  seten correctamente en cada epoca
-     * - Establecer la estrucutra de acuerdo al paper
-     * - Generar y mostrar las mejores soluciones de acuerdo a la epoca y evolución
-     * - Este paper no trae mutación quizá no es necesaria
-     * - Plotear resultados de cada epoca para la presentación final (ideal: lunes ´todo´)
-     */
 }
