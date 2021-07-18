412220944fde878e6346091ab800b57492d31601
morocco152 <58355129+morocco152@users.noreply.github.com>
2021-07-18 16:26:12 -0500

    Update Individuo.cpp

--- quantum/Individuo.cpp
+++ quantum/Individuo.cpp
@@ -3,11 +3,11 @@ using namespace std;
 
 //initial data
 vector<int> profit_vector = {12, 34, 23, 10, 12, 34, 8, 12, 54, 12};
-vector<int> weight_vector = {11, 12, 34, 22, 11, 17, 10, 11, 32, 21};
-const int sum_weight = 181;
+vector<int> weight_vector = {5, 7, 1, 4, 2, 3, 2, 1, 4, 3};
+const int sum_weight = 32;
 
 Individuo::Individuo(size_t size)
-    : size{size}, cromosoma{new Qubit[size]}, fitness{0.f}, profit{0}, cromo_bit{new int[size]} {}
+    : size{size}, cromosoma{new Qubit[size]}, fitness{0.f}, profit{0}, cromosome_colapsed{new int[size]} {}
 
 bool Individuo::operator < (const Individuo& o) const {
 	return (fitness > o.fitness);
@@ -17,9 +17,13 @@ Qubit& Individuo::operator[](int index){
     return cromosoma[index];
 }
 
+const int& Individuo::operator[](int index) const{
+    return cromosome_colapsed[index];
+}
+
 void Individuo::observate(){
     for (size_t i = 0; i < size; ++i)
-        cout << cromosoma[i].colapso() << "  ";
+        cout << cromosome_colapsed[i] << "  ";
     cout << endl;
 }
 
@@ -28,7 +32,7 @@ void Individuo::evaluate_fitness(const int marx_capacity){
     int total_weight = 0;
     for (size_t i = 0; i < size; ++i){
         int aux = cromosoma[i].colapso();
-        cromo_bit[i] = aux;
+        cromosome_colapsed[i] = aux;
         if (aux == 1){
             total_profit += profit_vector[i];
             total_weight += weight_vector[i];
@@ -39,10 +43,3 @@ void Individuo::evaluate_fitness(const int marx_capacity){
     weight = total_weight;   
     fitness = profit - (sum_weight*abs(weight-marx_capacity));
 }
-
-Individuo::~Individuo(){
-    delete[] cromosoma;
-}
-
-
-
