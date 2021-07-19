df659ed361180525a5cad8f7819da35393ea603a
morocco152 <58355129+morocco152@users.noreply.github.com>
2021-07-18 21:08:27 -0500

    Update Genetic.cpp

--- quantum/Genetic.cpp
+++ quantum/Genetic.cpp
@@ -8,16 +8,17 @@ void Genetic::initialize_Population(){
 
 void Genetic::make(){
     for (size_t i = 0; i < population_size; ++i)
-        population[i].observate();
+        population[i].observate(max_capacity);
 }
 
 void Genetic::repair(){
-    /* bustamante */
+    for (size_t i = 0; i < population_size; ++i)
+        population[i].Individuo::repair(max_capacity);
 }
 
 void Genetic::evaluate(){
     for (size_t i = 0; i < population_size; ++i)
-        population[i].evaluate_fitness(max_capacity);
+        population[i].evaluate_fitness(ty_fitness, max_capacity);
     sort(population.begin(), population.end());
 }
 
@@ -31,7 +32,7 @@ float Genetic::get_rotation_angle(const int xi,const int bi, bool condition){
         return 0; 
 }
 
-void Genetic::quantum_gate(Individuo &initial, Individuo &best){
+void Genetic::update(Individuo &initial, Individuo &best){
     const float pi = acosf(-1);
     bool condition = initial.fitness >= best.fitness;
     for (size_t i = 0; i < initial.size; ++i){
@@ -46,27 +47,33 @@ void Genetic::quantum_gate(Individuo &initial, Individuo &best){
     }
 }
 
-Genetic::Genetic(size_t popu_size, size_t cromo_size, size_t generations, const int max_capacity)
-    : population_size{popu_size}, cromosoma_size{cromo_size}, generations{generations}, max_capacity{max_capacity} { 
+Genetic::Genetic(size_t popu_size, size_t cromo_size, size_t generations, const int max_capacity, const int type)
+    : population_size{popu_size}, cromosoma_size{cromo_size}, generations{generations}, max_capacity{max_capacity}, ty_fitness{type} { 
         population.reserve(population_size);
     }
 
 void Genetic::exe(){
     initialize_Population();
+    make();
+    if (ty_fitness == 2) repair();
     evaluate();
     Individuo best = population[0];
     for (int i = 0; i < generations; ++i){
+        make();
+        if (ty_fitness == 2) repair();
         evaluate();
         for (int j = 0; j < population_size; ++j)
-            quantum_gate(population[j],best);
+            update(population[j],best);
         evaluate();
-        if (population[0].fitness > best.fitness)
-             best = population[0];
-        cout << "Generation: " << i << "  fitness: " << best.fitness << "  >>" << endl;
+        repair();
+        if (population[0].fitness > best.fitness) best = population[0];
+        //cout << "Generation: " << i << "  fitness: " << best.fitness <<" weight: "<< best.weight << "  >>" << endl;
     }
     cout << "Best Solution:" << endl;
-    best.observate();
-    cout << "Final weight: " << best.fitness << endl;
+    best.show_binary();
+    cout << "Final fitness: " << best.fitness << endl;
+    cout << "Final weight: " << best.weight << endl;
+    cout << "Knapsack capacity used: " << best.weight*100/max_capacity << "% " << endl;
     EXIT_SUCCESS;
 }
 
