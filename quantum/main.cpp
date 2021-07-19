903ed3908e7f1e40bf7495d85e1f9017d5c864ad
morocco152 <58355129+morocco152@users.noreply.github.com>
2021-07-18 21:05:29 -0500

    Update main.cpp

--- quantum/main.cpp
+++ quantum/main.cpp
@@ -4,11 +4,23 @@
 int main(int argc, char **argv)
 {
     //Algorithm
-    srand(static_cast<unsigned int>(time(nullptr)));
-    Genetic a(5,10,1000,20);
-    a.exe();
+    // fitness penality 1 
+    // fitness repair 2 
 
+    puts("fitness : Penalty!");    
+    srand(static_cast<unsigned int>(time(nullptr)));
+    Genetic a(20,50,1000,300,1);
+    a.exe();
     //free memory
     a.~Genetic();
+
+    puts("fitness : Repair");
+    srand(static_cast<unsigned int>(time(nullptr)));
+    Genetic b(20,50,1000,300,2);
+    b.exe();
+    //free memory
+    b.~Genetic();
+
     return EXIT_SUCCESS;
 }
+
